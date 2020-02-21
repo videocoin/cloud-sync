f8eaadbf1f98d332b53a6936671c32b8cd034d91
Nikita Savik <nikita@liveplanet.net>
2020-02-22 00:25:30 +0300

    Fix deploy

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 GOOS?=linux
 GOARCH?=amd64
 
-GCP_PROJECT=videocoin-network
+GCP_PROJECT?=videocoin-network
 
 NAME=syncer
 VERSION=$$(git describe --abbrev=0)-$$(git rev-parse --abbrev-ref HEAD)-$$(git rev-parse --short HEAD)
--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,6 +6,7 @@ readonly CHART_DIR=./deploy/helm
 CONSUL_ADDR=${CONSUL_ADDR:=127.0.0.1:8500}
 ENV=${ENV:=dev}
 VERSION=${VERSION:=`git describe --abbrev=0`-`git rev-parse --abbrev-ref HEAD`-`git rev-parse --short HEAD`}
+GCP_PROJECT=${GCP_PROJECT:=videocoin-network}
 
 function log {
   local readonly level="$1"
@@ -59,6 +60,7 @@ function deploy() {
     helm upgrade \
         --kube-context "${KUBE_CONTEXT}" \
         --install \
+        --set image.repository="gcr.io/${GCP_PROJECT}/${CHART_NAME}" \
         --set image.tag="${VERSION}" \
         --set config.bucket="${BUCKET}" \
         --set secrets.mqUri="${MQ_URI}" \
